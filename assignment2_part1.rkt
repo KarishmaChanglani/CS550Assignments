ac5333d8562c698e71163b9a35da22c3e51cb7cb
Patrick Brinich <35614716+pjb338@users.noreply.github.com>
2018-02-10 23:36:43 -0500

    Added tests to part 1, added part 2 except for memoization

--- assignment2_part1.rkt
+++ assignment2_part1.rkt
@@ -2,20 +2,37 @@
 (require racket/stream)
 (require math/number-theory)
 (require rackunit)
-
+(require rackunit/text-ui)
 (require racket/trace)
 
 (define (stream-enumerate-interval low high)
-(if (> low high)
-empty-stream
-(stream-cons
-low
-(stream-enumerate-interval (+ low 1) high))))
+  (if (> low high)
+      empty-stream
+      (stream-cons low (stream-enumerate-interval (+ low 1) high))))
+
+
+;Checks
+(define-test-suite stream-enumerate-interval-test-suite
+  ;Bounds check
+  (check-equal? (stream-enumerate-interval 10 1) empty-stream "pos low > pos high is empty-stream")
+  (check-equal? (stream-enumerate-interval -1 -10) empty-stream "neg low > neg high is empty-stream")
+  (check-equal? (stream-enumerate-interval 10 -10) empty-stream "neg low > neg high is empty-stream")
+
+  ;Single element check
+  (check-equal? (let ((teststream (stream-enumerate-interval 10 10))) (and (= 10 (stream-first teststream)) (stream-empty? (stream-rest teststream)))) #t  "low = high generates only low")
+
+  ;Correctness check
+  (check-equal? (stream->list (stream-enumerate-interval 0 10)) (list 0 1 2 3 4 5 6 7 8 9 10) "[0,10] generates correct output")
+  (check-equal? (stream->list (stream-enumerate-interval -10 0)) (list -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0) "[-10,0] generates correct output")
+  )
+(run-tests stream-enumerate-interval-test-suite 'verbose)
 
 (trace stream-enumerate-interval)
-(stream-first
-(stream-rest
- (stream-filter prime?
- (stream-enumerate-interval 10000 1000000))))
-
-
+(define-test-suite part-1-test-suite
+  (check-equal? (stream-first
+                 (stream-rest
+                  (stream-filter prime?
+                                 (stream-enumerate-interval 10000 1000000)))) 10009 "The second prime after 10000 is 10009")
+  ; N. J. A. Sloane, "Table of n, prime(n) for n = 1..100000", Online Encyclopedia of Integer Sequences, 2018. [Online]. Available: http://oeis.org/A000040/a000040.txt.  [Accessed: Feb 10, 2018].
+  )
+(run-tests part-1-test-suite 'verbose)
---
+++ test2.rkt
@@ -0,0 +1,66 @@
+#lang racket
+(require rackunit)
+(require rackunit/text-ui)
+(require "mceval.rkt")
+
+(define (test-mceval exp)
+  (with-handlers ([exn:fail? (lambda (exn) exn)])
+    (mceval exp (setup-environment))))
+
+
+(define delay-force-tests
+  (test-suite
+   "Test delay and force"
+   (check-equal? (test-mceval '(begin (delay (error)) 3))
+                 3
+                 "(begin (delay (error)) 3)")
+   (check-equal? (test-mceval '(force (delay 3)))
+                 3
+                 "(force (delay 3))")
+   (check-equal? (test-mceval '((delay 5)))
+                 5
+                 "((delay 5))")
+   (check-equal? (test-mceval '(let ((x (delay 3))) (force x)))
+                 3
+                 "(let ((x (delay 3))) (force x))")
+   (check-equal? (test-mceval '(force (delay (force (delay 3)))))
+                 3
+                 "(force (delay (force (delay 3))))")
+   (check-equal? (test-mceval '(let ((x (delay (+ 1 2)))) (+ (force x) (force x))))
+                 6
+                 "(let ((x (delay (+ 1 2)))) (+ (force x) (force x)))")
+   (check-equal? (test-mceval '(let ((x 0))
+                                 (let ((y (delay (begin (set! x (+ x 1)) x))))
+                                   (+ (force y) (force y)))))
+                 2
+                 "Delayed expression with side-effect")))
+
+(define stream-tests
+  (test-suite
+   "Test streams"
+   (check-equal? (test-mceval '(stream-empty? empty-stream))
+                 #t
+                 "(stream-empty? empty-stream)")
+   (check-equal? (test-mceval '(stream-first (stream-cons 1 empty-stream)))
+                 1
+                 "(stream-first (stream-cons 1 empty-stream))")
+   (check-equal? (test-mceval '(stream-empty? (stream-rest (stream-cons 1 empty-stream))))
+                 #t
+                 "(stream-empty? (stream-rest (stream-cons 1 empty-stream)))")
+   (check-equal? (test-mceval '(stream-first (stream-cons 1 (error))))
+                 1
+                 "(stream-first (stream-cons 1 (error)))")
+   (check-equal? (test-mceval '(stream-first (stream-cons (+ 2 3) (stream-cons (+ 2 3) (error)))))
+                 5
+                 "(stream-first (stream-cons (+ 2 3) (stream-cons (+ 2 3) (error))))")
+   (check-equal? (test-mceval '(stream-first (stream-rest (stream-cons (+ 2 3) (stream-cons (+ 2 3) (error))))))
+                 5
+                 "(stream-first (stream-rest (stream-cons (+ 2 3) (stream-cons (+ 2 3) (error)))))")))
+
+(define part2-test
+  (test-suite "Test for Part 2"
+  (check-equal? (test-mceval '(stream-first (stream-rest (stream-filter prime? (stream-enumerate-interval 10000 1000000))))) 10009 "Second prime past 10000 is 10009")))
+  
+(run-tests delay-force-tests)
+(run-tests stream-tests)
+(run-tests part2-test)
